49a96aab87b705062e7ef10d4abd0945643be733
Aswal <Aswal@ASWAL-PC>
2017-02-12 10:57:21 +0530

    Basic Change

--- SaniShop/Controllers/AccountController.cs
+++ SaniShop/Controllers/AccountController.cs
@@ -42,6 +42,7 @@ namespace SaniShop.Controllers
 
         public ApplicationUserManager UserManager
         {
+           
             get
             {
                 return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
@@ -73,6 +74,8 @@ namespace SaniShop.Controllers
                 return View(model);
             }
 
+
+            var Data = "Test";
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
